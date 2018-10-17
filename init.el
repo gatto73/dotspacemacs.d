cd6187f527d0d43dedf78a1e01c4980e29118899
Gatto Silvestro <gatto73silvestro@gmail.com>
2018-10-17 10:37:30 +0200

    Change version-control-diff-side to right

--- init.el
+++ init.el
@@ -53,7 +53,7 @@ This function should only modify configuration layer settings."
      git
      (version-control :variables
                       version-control-diff-tool 'diff-hl
-                      version-control-diff-side 'left
+                      version-control-diff-side 'right
                       version-control-global-margin t)
      (c-c++ :variables
             c-c++-default-mode-for-headers 'c++-mode

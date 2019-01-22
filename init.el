cbd24e248208d3644a007e2f7e9e2a88dc2db35f
Gatto Z-Book <gatto73silvestro@gmail.com>
2019-01-22 11:13:36 +0100

    Improve auto-completion configuration.

--- init.el
+++ init.el
@@ -49,13 +49,13 @@ This function should only modify configuration layer settings."
      (auto-completion :variables
                       auto-completion-return-key-behavior 'complete
                       auto-completion-tab-key-behavior 'cycle
-                      auto-completion-complete-with-key-sequence nil
-                      auto-completion-complete-with-key-sequence-delay 0.1
+                      auto-completion-complete-with-key-sequence "jj"
+                      auto-completion-complete-with-key-sequence-delay 0.3
                       auto-completion-idle-delay 0.2
                       auto-completion-private-snippets-directory nil
-                      auto-completion-enable-snippets-in-popup nil
-                      auto-completion-enable-help-tooltip nil
-                      auto-completion-enable-sort-by-usage nil)
+                      auto-completion-enable-snippets-in-popup t
+                      auto-completion-enable-help-tooltip t
+                      auto-completion-enable-sort-by-usage t)
      markdown
      html
      csv
